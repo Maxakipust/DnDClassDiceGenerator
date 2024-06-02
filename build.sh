225648386fda9a9605db6f7646d47b5320281004
Max Kipust <maxakipust@gmail.com>
2024-06-02 14:44:36 -0500

    DnDClassDiceGenerator initial commit Added a font that contains DnD class symbols Added a config to generate d20s with each symbol Added a script to build a stl file for each d20

---
+++ build.sh
@@ -0,0 +1,49 @@
+# set -o xtrace
+
+VERSION="2.3"
+
+echo "====Checking dependencies===="
+
+for font in "MyDNDFont" "Luminari"; do
+    if fc-list | grep -q $font; then
+        echo "Found font: ${font}"
+    else
+        echo "Missing font: ${font}"
+        echo "install missing fonts by running: \"sudo cp fonts/* /usr/share/fonts && sudo fc-cache -f -v\""
+        exit 1
+    fi
+done
+
+if openscad --version > /dev/null; then
+    echo "Found openscad"
+else
+    echo "Missing openscad"
+    echo "Install from https://openscad.org/"
+    exit 1
+fi
+
+echo "====All dependencies found===="
+
+mkdir -p build/${VERSION}
+
+for class in "Barb" "Druid" "Pal" "Bard" "Fighter" "Warlock" "Cleric" "Monk" "Rogue" "Wiz" "DND" "Range" "Sorc"; do
+    echo "Starting ${class}"
+    openscad \
+    --export-format binstl -o "build/${VERSION}/${class}_${VERSION}.stl" \
+    -p DnDClassDiceGenerator.json -P ${class} \
+    PolyDiceGenerator.scad > "build/${VERSION}/${class}_${VERSION}.log" 2>&1 &
+done
+wait
+echo "Finished"
+
+
+echo "Cleaning up logs"
+logfile=build/${VERSION}/build.log
+touch $logfile
+for class in "Barb" "Druid" "Pal" "Bard" "Fighter" "Warlock" "Cleric" "Monk" "Rogue" "Wiz" "DND" "Range" "Sorc"; do
+    echo "====${class}====" >> $logfile
+    cat build/${VERSION}/${class}_${VERSION}.log >> $logfile
+    echo -e "\n\n" >> $logfile
+    rm build/${VERSION}/${class}_${VERSION}.log
+done
+echo "Finished"
